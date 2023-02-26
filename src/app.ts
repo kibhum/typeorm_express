086f8c01ecc908d0b024c9185cc1b365d57c80c7
kibethh <kibethh@gmail.com>
2023-02-26 14:54:24 +0300

    parsed swagger yaml file

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,17 @@
+import fs from "fs";
 import express, { Express } from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yaml";
 
 import { taskRouter } from "./tasks/routes/task.router";
 
 const app: Express = express();
+const file = fs.readFileSync("./swagger.yaml", "utf8");
+const swaggerDocument = YAML.parse(file);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 // Parsing the request body
 app.use(bodyParser.json());
 // Cors
